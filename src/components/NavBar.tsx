40b46e73b0745a98fef4dc6372a4ef17610a9147
eunkyeng_shin <eunkyeng_shin@tmax.co.kr>
2022-07-06 19:26:45 +0900

    [feat] nav영역 css추가및 페이지 전체 배경색 추가

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,66 +2,35 @@ import Link from 'next/link';
 import Image from 'next/image';
 import logo from '../../public/logo.png';
 import { useRouter } from 'next/router';
-
+import navbar from '../styles/NavBar.module.css';
 export default function NavBar() {
   const router = useRouter();
   //location에 관련된 데이터를 알 수 있음.
   return (
-    <div className="nav-wrapper">
-      <nav>
-        <Link href="/" passHref>
-          <Image className="logo" src={logo} alt="Picture of the author" />
-        </Link>
-        <div className="link-wrapper">
-          <Link href="/map">
-            <a className={router.pathname === '/map' ? 'active' : ''}>MAP </a>
-          </Link>
-          <Link href="/history">
-            <a className={router.pathname === '/history' ? 'active' : ''}>HISTORY </a>
-          </Link>
-          <Link href="/game">
-            <a className={router.pathname === '/game' ? 'active' : ''}>GAME </a>
-          </Link>
-          <Link href="/notice">
-            <a className={router.pathname === '/notice' ? 'active' : ''}>NOTICE </a>
-          </Link>
+    <div>
+      <nav className={navbar.nav}>
+        <div className={navbar.linkWrapper}>
+          <div className={navbar.ImgLink}>
+            <Link href="/" passHref>
+              <Image src={logo} alt="Picture of the author" />
+            </Link>
+          </div>
+          <div className={navbar.linkList}>
+            <Link href="/map">
+              <a className={router.pathname === '/map' ? navbar.active : ''}>MAP </a>
+            </Link>
+            <Link href="/history">
+              <a className={router.pathname === '/history' ? navbar.active : ''}>HISTORY </a>
+            </Link>
+            <Link href="/game">
+              <a className={router.pathname === '/game' ? navbar.active : ''}>GAME </a>
+            </Link>
+            <Link href="/notice">
+              <a className={router.pathname === '/notice' ? navbar.active : ''}>NOTICE </a>
+            </Link>
+          </div>
         </div>
       </nav>
-
-      <style jsx>
-        {`
-          .nav-wrapper {
-            width: 100%;
-            position: absolute;
-            top: 0;
-            left: 0;
-          }
-          nav {
-            height: 40px;
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            padding: 0 200px;
-            max-width: 1200xp;
-          }
-          a {
-            font-weight: bold;
-            font-size: 20px;
-          }
-          .active {
-            color: #f6681c;
-          }
-          .link-wrapper {
-            display: flex;
-            gap: 40px;
-          }
-          .logo {
-            width: 250px;
-            height: 150px;
-            top: 25px;
-          }
-        `}
-      </style>
     </div>
   );
 }
